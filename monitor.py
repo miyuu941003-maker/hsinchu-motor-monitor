d804fdac26ebcb11579dce1b849337cdf7e80b1e
miyuu941003-maker <miyuu.941003@gmail.com>
2026-07-29 09:52:39 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -5,23 +5,24 @@ URL = "https://www.mvdis.gov.tw/m3-emv-trn/exm/locations"
 
 def check_booking():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(
+            headless=True,
+            args=["--disable-blink-features=AutomationControlled"]
+        )
+
         page = browser.new_page()
 
         page.goto(
             URL,
-            wait_until="domcontentloaded",
-            timeout=60000
+            wait_until="commit",
+            timeout=90000
         )
 
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(10000)
 
         text = page.locator("body").inner_text()
 
-        if "額滿" not in text:
-            print("可能有名額！")
-        else:
-            print("目前沒有名額")
+        print(text[:500])
 
         browser.close()
 
